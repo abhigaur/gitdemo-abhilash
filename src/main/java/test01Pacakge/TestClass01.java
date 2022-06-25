a46f7a4ddc95265f0afbda577d6a582ff3dc2cc5
Abhilash Gaur <abhigaur100@gmail.com>
2022-06-25 23:52:16 +0530

    adding other file

---
+++ src/main/java/test01Pacakge/TestClass01.java
@@ -0,0 +1,12 @@
+package test01Pacakge;
+
+public class TestClass01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("test file 01");
+
+	}
+
+}
