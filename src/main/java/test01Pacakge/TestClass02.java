2a596d25ac143742023bb0e8524d65d43730e152
Abhilash Gaur <abhigaur100@gmail.com>
2022-06-26 13:05:40 +0530

    commit done by sdet2

---
+++ src/main/java/test01Pacakge/TestClass02.java
@@ -0,0 +1,11 @@
+package test01Pacakge;
+
+public class TestClass02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("test file 02 done by sdet 2");
+
+	}
+
+}
