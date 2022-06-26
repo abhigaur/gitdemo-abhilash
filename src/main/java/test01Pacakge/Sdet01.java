9ef07535776ef78dda3ea9e5b036152662a4ace7
Abhilash Gaur <abhigaur100@gmail.com>
2022-06-26 12:54:55 +0530

    commit abhi sdet1

---
+++ src/main/java/test01Pacakge/Sdet01.java
@@ -0,0 +1,12 @@
+package test01Pacakge;
+
+public class Sdet01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("this id sdet 01 in prodject ");
+
+	}
+
+}
